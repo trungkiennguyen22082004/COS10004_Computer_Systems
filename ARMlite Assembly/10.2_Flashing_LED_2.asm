1bc58b6d22f570893aa9059b7dd4e526501df564
MOON <114036681+moon22082004@users.noreply.github.com>
2022-10-25 23:19:30 +1100

    Update 10.2_Flashing_LED_2.asm

--- ARMlite Assembly/10.2_Flashing_LED_2.asm
+++ ARMlite Assembly/10.2_Flashing_LED_2.asm
@@ -1,31 +1,35 @@
 // By MOON
       MOV R0, #.white
       MOV R1, #.red
-flashon:
+flash:
+      BL oneseconddelay
       STR R1, .Pixel335
-      LDR R3, .Time
-ondelay:
-      LDR R4, .Time
-      SUB R5, R4, R3
-      CMP R5, #1
-      BLT ondelay
+      BL oneseconddelay
       STR R0, .Pixel335
-      LDR R3, .Time
-      ADD R2, R2, #1
-      CMP R2, #3
-      BEQ delay
-offdelay:
-      LDR R4, .Time
-      SUB R5,R4,R3
-      CMP R5, #1
-      BLT offdelay
-      B flashon
-delay:
-      LDR R4, .Time
-      SUB R5,R4,R3
-      CMP R5, #2
-      BLT delay
-      LDR R3, .Time
-      MOV R2, #0
-      B offdelay
+      ADD R3, R3, #1
+      CMP R3, #3
+      BLT flash
+      BL twosecondsdelay
+      MOV R3, #0
+      B flash
       HALT
+oneseconddelay:
+      PUSH {R4, R5, R6}
+      LDR R6, .Time
+loop1:
+      LDR R4, .Time
+      SUB R5, R4, R6
+      CMP R5, #1
+      BLT loop1
+      POP {R4, R5, R6}
+      RET
+twosecondsdelay:
+      PUSH {R4, R5, R6}
+      LDR R6, .Time
+loop3:
+      LDR R4, .Time
+      SUB R5, R4, R6
+      CMP R5, #2
+      BLT loop3
+      POP {R4, R5, R6}
+      RET
