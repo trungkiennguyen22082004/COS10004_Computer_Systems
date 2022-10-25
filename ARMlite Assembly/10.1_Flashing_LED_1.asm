09050481cd49c059c09c6fe466f6e541eb5c3953
MOON <114036681+moon22082004@users.noreply.github.com>
2022-10-25 23:19:13 +1100

    Update 10.1_Flashing_LED_1.asm

--- ARMlite Assembly/10.1_Flashing_LED_1.asm
+++ ARMlite Assembly/10.1_Flashing_LED_1.asm
@@ -1,20 +1,20 @@
 // By MOON
       MOV R0, #.white
       MOV R1, #.red
-flashon:
-      STR R0, .Pixel335
-      LDR R3, .Time
-ondelay:
-      LDR R4, .Time
-      SUB R5, R4, R3
-      CMP R5, #1
-      BLT ondelay
+flash:
+      BL delay
       STR R1, .Pixel335
-      LDR R3, .Time
-offdelay:
-      LDR R4, .Time
-      SUB R5,R4,R3
-      CMP R5, #1
-      BLT offdelay
-      B flashon
+      BL delay
+      STR R0, .Pixel335
+      B flash
       HALT
+delay:
+      PUSH {R4, R5, R6}
+      LDR R6, .Time
+loop:
+      LDR R4, .Time
+      SUB R5, R4, R6
+      CMP R5, #1
+      BLT loop
+      POP {R4, R5, R6}
+      RET
